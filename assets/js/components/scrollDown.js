033a0f78186f05ee3c1b73cb4a8996033620090a
Serhii Koziar <catalonya1111@gmail.com>
2021-03-10 14:57:15 +0200

    fix scroll

--- assets/js/components/scrollDown.js
+++ assets/js/components/scrollDown.js
@@ -1,6 +1,5 @@
 export function scrollDown() {
   let arrow = document.querySelector('.scrollDown')
-  let windowHeight = window.innerHeight;
   arrow.addEventListener('click', (e) => {
     scrollBy({
       top: e.target.closest('section').nextElementSibling.getBoundingClientRect().top,
