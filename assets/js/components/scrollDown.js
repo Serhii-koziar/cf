7f20ff2dbdb055ad78bc00013a2df1162458ea18
Serhii Koziar <catalonya1111@gmail.com>
2021-03-10 14:56:53 +0200

    fix scrollBot

--- assets/js/components/scrollDown.js
+++ assets/js/components/scrollDown.js
@@ -1,9 +1,9 @@
 export function scrollDown() {
   let arrow = document.querySelector('.scrollDown')
   let windowHeight = window.innerHeight;
-  arrow.addEventListener('click', () => {
+  arrow.addEventListener('click', (e) => {
     scrollBy({
-      top: windowHeight,
+      top: e.target.closest('section').nextElementSibling.getBoundingClientRect().top,
       left:0,
       behaviour: "smooth"
     })
